f53a77dd099a902eb383590d209f4ae008a3f749
makauj <makauwanyoike@gmail.com>
2024-12-03 08:47:19 +0300

    binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - 
+ * @parent: pointer to the parent node
+ * @value: value to add to new node
+ *
+ * Return: pointer to new node, else NULL
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (!parent || !new_node)
+		return NULL;
+}
